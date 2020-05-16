5f81a6dc3829b47a9ff6e3be6a0eb1cd372c11f6
AidenMei1102 <aidenmei@users.noreply.github.com>
2020-05-15 21:51:51 -0400

    add speak button , circle  and fix background color

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+
+
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
 var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', ' green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', ' purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
 var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
+
+
+var col = "";
+
 
 var recognition = new SpeechRecognition();
 var speechRecognitionList = new SpeechGrammarList();
@@ -14,20 +20,43 @@ recognition.lang = 'en-US';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
-var diagnostic = document.querySelector('.output');
-var bg = document.querySelector('html');
+var diagnostic = document.querySelector('#circle');
+var bg = document.querySelector('#circle');
 var hints = document.querySelector('.hints');
+
+
+var diameter = window.prompt("Enter the diameter of the circle");
+
+if(diameter > 600 || diameter < 2 ){
+
+
+  alert("Diameter should be in range 2 to 600");
+
+  diameter = 300;
+
+}else{
+
+draw();
+
+}
 
 var colorHTML= '';
 colors.forEach(function(v, i, a){
   console.log(v, i);
   colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
 });
-hints.innerHTML = 'Tap/click then say a "color" to change the background color';
+//hints.innerHTML = 'Tap/click then say a "color" to change the background color';
 
-document.body.onclick = function() {
+var but = document.querySelector(".btn")
+
+but.onclick = function() {
   recognition.start();
-  console.log('Ready to receive a color command.');
+ // console.log('Ready to receive a color command.');
+
+  but.innerHTML = "Stop"
+ // alert("this was not working")
+
+
 }
 
 recognition.onresult = function(event) {
@@ -40,19 +69,103 @@ recognition.onresult = function(event) {
   // The second [0] returns the SpeechRecognitionAlternative at position 0.
   // We then return the transcript property of the SpeechRecognitionAlternative object
   var color = event.results[0][0].transcript;
-  diagnostic.textContent = 'Result received: ' + color + '.';
-  bg.style.backgroundColor = color;
+  //diagnostic.textContent = 'Result received: ' + color + '.';
+ // bg.style.backgroundColor = color;
+ but.innerHTML = "Stop";
+
+
+
+    var mColor = "";
+  
+     if(color.includes("background")){
+  /// set background
+    
+   
+     mColor = color.substring(10, color.length)
+     //alert(mColor)
+      diagnostic.style.backgroundColor = mColor;
+  
+      
+  
+     }else if(color.includes("size")){
+  // set size
+  //alert("Size")
+   mColor = color.substring(4, color.length)
+  
+     diameter = mColor;
+  
+     draw();
+  
+     }else if(color.includes("color")){
+  /// set color of circle
+  //alert("Color")
+   mColor = color.substring(5, color.length)
+  
+  
+  col = mColor;
+  
+      draw();
+  
+     }else if(color.includes("help")){
+  
+  /// user needs help
+  //alert("help")
+   mColor = color.substring(4, color.length)
+  /// i will show options here.. 
+       
+  
+  
+     }
   console.log('Confidence: ' + event.results[0][0].confidence);
 }
 
 recognition.onspeechend = function() {
   recognition.stop();
+  but.innerHTML = "Stop";
 }
 
 recognition.onnomatch = function(event) {
   diagnostic.textContent = "I didn't recognise that color.";
+  but.innerHTML = "Stop";
 }
 
 recognition.onerror = function(event) {
+  but.innerHTML = "Stop";
   diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function draw() {
+var canvas = document.getElementById('circle');
+
+var ctx = canvas.getContext('2d'); 
+var X = canvas.width / 2;
+var Y = canvas.height / 2;
+var R = diameter/2;
+ctx.beginPath();
+ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+ctx.lineWidth = 3;
+ctx.strokeStyle = "#ccdddd";
+ctx.fillStyle = col;
+ctx.stroke()
+ctx.fill();
+
+  }
+
+
